aa5fd0feeb831f68d107083a6d7fff4dd5cea37d
SKurbatova <skurbatova@cbgr.ru>
2022-11-18 15:44:42 +0300

    add emulator

--- untitled/lib/main.dart
+++ untitled/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Первое'),
+      home: MyHomePage(title: 'Второе   '),
     );
   }
 }
