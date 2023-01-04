95d85aff608f843fbd0eb55c67ecbc044d4c02cd
SKurbatova <fairytellsblog@gmail.com>
2023-01-04 15:13:40 +0300

    upd

--- untitled/lib/main.dart
+++ untitled/lib/main.dart
@@ -3,91 +3,114 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'dart:ui';
 
+//Выполнение приложений на Flutter начинается с функции Main
+
 void main() {
-  // runApp(MyApp());
   runApp(
-    Center(
-      child: Text(
-        "RECRUTER",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-          fontSize: 50,
-          background: Paint()..color = Colors.blue,
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.greenAccent,
+        appBar: AppBar(
+          title: Text("HRecruter"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text(
+            "Домашная страница",
+            style: TextStyle(
+                fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
+          ),
         ),
       ),
     ),
   );
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Второе   '),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Нажали на кнопку много раз:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Text(
-              'А вот тут мы добавили картинку!',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Container(
-              color: Colors.white,
-              child: Image.network("https://picsum.photos/250?image=9"),
-            ),
-            Container(
-              color: Colors.white,
-              child: Image.asset("/owl.jpg"),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
+// void main() {
+//   // runApp(MyApp());
+//   runApp(
+//     Center(
+//       child: Text(
+//         "RECRUTER",
+//         textDirection: TextDirection.ltr,
+//         style: TextStyle(
+//           fontSize: 50,
+//           background: Paint()..color = Colors.blue,
+//         ),
+//       ),
+//     ),
+//   );
+// }
+//
+// class MyApp extends StatelessWidget {
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: MyHomePage(title: 'Второе   '),
+//     );
+//   }
+// }
+//
+// class MyHomePage extends StatefulWidget {
+//   MyHomePage({Key key, this.title}) : super(key: key);
+//
+//   final String title;
+//
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//   int _counter = 0;
+//
+//   void _incrementCounter() {
+//     setState(() {
+//       _counter++;
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Text(
+//               'Нажали на кнопку много раз:',
+//             ),
+//             Text(
+//               '$_counter',
+//               style: Theme.of(context).textTheme.headline4,
+//             ),
+//             Text(
+//               'А вот тут мы добавили картинку!',
+//               style: Theme.of(context).textTheme.headline4,
+//             ),
+//             Container(
+//               color: Colors.white,
+//               child: Image.network("https://picsum.photos/250?image=9"),
+//             ),
+//             Container(
+//               color: Colors.white,
+//               child: Image.asset("/owl.jpg"),
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: Icon(Icons.add),
+//       ), // This trailing comma makes auto-formatting nicer for build methods.
+//     );
+//   }
+// }
