9da37c73a6db5fa3bfb3d5a6e7ce172897252efd
SKurbatova <fairytellsblog@gmail.com>
2023-01-08 01:21:00 +0300

    sum

--- untitled/lib/Widgets/auth/loginPage.dart
+++ untitled/lib/Widgets/auth/loginPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/Widgets/auth/regPage.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({Key key}) : super(key: key);
@@ -12,29 +13,31 @@ class SettingsPage extends StatelessWidget {
                 child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      const Text('Settings Page'),
-                      TextButton.icon(
-                        style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all(Colors.blue),
-                            foregroundColor:
-                            MaterialStateProperty.all(Colors.deepPurpleAccent)),
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        icon: const Icon(Icons.arrow_back),
-                        label: const Text('Назад'),
-                      ),
-                      TextButton.icon(
-                        style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all(Colors.blue),
-                            foregroundColor:
-                            MaterialStateProperty.all(Colors.deepPurpleAccent)),
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        icon: const Icon(Icons.arrow_back),
-                        label: const Text('перейти куда-то еще'),
-                      ),
-                    ]))));
+              const Text('Settings Page'),
+              TextButton.icon(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.blue),
+                    foregroundColor:
+                        MaterialStateProperty.all(Colors.deepPurpleAccent)),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: const Icon(Icons.arrow_back),
+                label: const Text('Назад'),
+              ),
+              TextButton.icon(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.blue),
+                    foregroundColor:
+                        MaterialStateProperty.all(Colors.deepPurpleAccent)),
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return const RegPage();
+                  }));
+                },
+                icon: const Icon(Icons.arrow_back),
+                label: const Text('Регистрация'),
+              ),
+            ]))));
   }
 }
--- untitled/lib/main.dart
+++ untitled/lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'dart:ui';
 import 'Widgets/auth/loginPage.dart';
+import 'Widgets/auth/regPage.dart';
 
 //Выполнение приложений на Flutter начинается с функции Main
 
@@ -11,7 +11,8 @@ void main() {
     initialRoute: '/',
     routes: {
       '/': (context) => const Main(),
-      '/settings': (context) => const SettingsPage()
+      '/settings': (context) => const SettingsPage(),
+      '/registration': (context) => const RegPage()
     },
   ));
 }
@@ -22,29 +23,57 @@ class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.greenAccent,
-        body: SafeArea(
-            child: Center(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text('Main Page'),
-                      TextButton.icon(
-                        style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all(
-                                Colors.blue),
-                            foregroundColor:
-                            MaterialStateProperty.all(Colors.deepPurpleAccent)),
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/settings');
-                        },
-                        icon: const Icon(Icons.settings),
-                        label: const Text('Перейти в настройки'),
-                      ),
-                    ]))));
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Меню'),
+        backgroundColor: Colors.green[600],
+      ),
+      backgroundColor: Colors.greenAccent,
+      body: SafeArea(
+        child: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            const Text('Main Page'),
+            TextButton.icon(
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.blue),
+                  foregroundColor:
+                      MaterialStateProperty.all(Colors.deepPurpleAccent)),
+              onPressed: () {
+                Navigator.pushNamed(context, '/settings');
+              },
+              icon: const Icon(Icons.settings),
+              label: const Text('Перейти в настройки'),
+            ),
+          ]),
+        ),
+      ),
+      drawer: Drawer(
+        child: new ListView(
+          children: <Widget>[
+            new DrawerHeader(
+              margin: EdgeInsets.zero,
+              padding: EdgeInsets.zero,
+              child: UserAccountsDrawerHeader(
+                decoration: BoxDecoration(color: Colors.brown),
+                accountName: Text('Ярослава Х.'),
+                accountEmail: Text('yar@gmail.com'),
+                currentAccountPicture: Container(
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.lightGreen),
+                ),
+              ),
+            ),
+            new ListTile(
+              title: new Text('Личная страница'),
+              onTap: () {},
+            ),
+            new ListTile(
+              title: new Text('Настройки'),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
-
-
-
-
