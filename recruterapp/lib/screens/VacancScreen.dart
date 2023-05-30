dfdcf52628a6b119aeda002063b805b2ba257de1
SKurbatova <58175476+SofiFairyTell@users.noreply.github.com>
2023-05-30 23:35:32 +0300

    upd

--- recruterapp/lib/screens/VacancScreen.dart
+++ recruterapp/lib/screens/VacancScreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'DrawerWidget.dart';
 
 import 'Widgets/AppBarCustom.dart';
+import 'Widgets/CardWidget.dart';
 
 final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
@@ -9,6 +10,34 @@ class VacancScreen extends StatelessWidget {
   VacancScreen({Key key}) : super(key: key);
 
   final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  List<CardData> cardDataList = [
+    CardData(
+      title: 'Бизнес-аналитик',
+      description: 'Количество требуемых специалистовв. Требования. Тестовое',
+      imageUrl: 'assets/images/image1.jpg',
+    ),
+    CardData(
+      title: 'Jun-DEV',
+      description: 'Количество требуемых специалистовв. Требования. Тестовое',
+      imageUrl: 'assets/images/image2.jpg',
+    ),
+    CardData(
+      title: 'Senior-DEV',
+      description: 'Количество требуемых специалистовв. Требования. Тестовое',
+      imageUrl: 'assets/images/image3.jpg',
+    ),
+    CardData(
+      title: 'Архитектор',
+      description: 'Количество требуемых специалистовв. Требования. Тестовое',
+      imageUrl: 'assets/images/image3.jpg',
+    ),
+    CardData(
+      title: 'Тестировщик',
+      description: 'Количество требуемых специалистовв. Требования. Тестовое',
+      imageUrl: 'assets/images/image3.jpg',
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -21,26 +50,55 @@ class VacancScreen extends StatelessWidget {
           child: Column(
             children: [
               // Виджет 1 - ссылка на заглушку виджета с редактированием текущего экрана
-              Placeholder(
-                fallbackHeight: 100,
-              ),
-
+              buildCustomAppBarWithActions(),
               // Виджет 2 - область для листа из виджетов
-              ListView.builder(
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                itemCount: 10,
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                    title: Text('Item $index'),
-                  );
-                },
-              ),
+              buildMultipleCardWidgets(cardDataList),
             ],
           ),
         ),
       ),
       drawer: DrawerWidget(),
+    );
+  }
+
+  Widget buildMultipleCardWidgets(List<CardData> cardDataList) {
+    return Column(
+      children: cardDataList.map((cardData) {
+        return CardWidget(
+          title: cardData.title,
+          description: cardData.description,
+          imageUrl: cardData.imageUrl,
+        );
+      }).toList(),
+    );
+  }
+
+  PreferredSizeWidget buildCustomAppBarWithActions() {
+    return PreferredSize(
+      preferredSize: Size.fromHeight(kToolbarHeight),
+      child: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            toolbarHeight: kToolbarHeight,
+            actions: [
+              IconButton(
+                icon: Icon(Icons.add),
+                onPressed: () {
+                  // Заглушка для обработки нажатия на кнопку "Добавить"
+                  print('Нажата кнопка "Добавить"');
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {
+                  // Заглушка для обработки нажатия на кнопку "Искать"
+                  print('Нажата кнопка "Искать"');
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
